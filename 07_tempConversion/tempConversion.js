7b02f63f0093d30f0b0da15bab78558ae9a25202
Daniel Souza <danielrsouza10@gmail.com>
2023-04-22 11:51:56 -0300

    done again and now I have done with a more classy solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,17 +1,15 @@
-const convertToCelsius = function (farenheit) {
+const convertToCelsius = function(temperatureF) {
 
-  //Math.round(...*10)/10 to round to 1 decimal case
-  let celciusResult = Math.round(5 * ((farenheit - 32) / 9) * 10) / 10;
+    //convert using the formula and round 1 decial *((10)/10)
+  return Math.round(((temperatureF - 32) * (5/9))*10)/10;
 
-  //could put all the formula in the return to calculate
-  return celciusResult;
 };
 
-const convertToFahrenheit = function (celcius) {
- 
-  let farenheitResult = Math.round((celcius * (9 / 5) + 32) * 10) / 10;
+const convertToFahrenheit = function(temperatureC) {
 
-  return farenheitResult;
+    //convert using the formula and round 1 decial *((10)/10)
+  return Math.round(((temperatureC * (9/5)) + 32)*10)/10;
+
 };
 
 // Do not edit below this line
@@ -19,4 +17,3 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
-//c/5 = f-32/9
