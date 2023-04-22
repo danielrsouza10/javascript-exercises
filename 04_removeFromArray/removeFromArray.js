3764b231de13ee6c651bed9b3a31936d58e5f216
Daniel Souza <danielrsouza10@gmail.com>
2023-04-22 11:49:36 -0300

    Done again. Still struggling but I think I really understand the forEach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,18 @@
-const removeFromArray = function (myArray, ...args) {
-
-    const newArray = [];
-
-    myArray.forEach((item) => {
-        if(!args.includes(item)){
-            newArray.push(item);
-        }
-    })
+const removeFromArray = function(array, ...args) {
     
+    //cria uma array vazia para receber os items da matriz anterior
+    const myArray = [];
 
-    return newArray;
-
+    //A funcao forEach percorre todos os elementos presentes na matriz e executa a acao que for designada
+    array.forEach((item) => {
+        //percorre a matriz e adiciona todos os itens na nova matriz
+        //exceto (!args) os incluidos no parametro args recebido pela funcao
+        if(!args.includes(item)){
+            //adiciona os elementos na matriz
+            myArray.push(item);
+        }
+    })  //retorno da funcao
+        return myArray;
 };
 
 // Do not edit below this line
